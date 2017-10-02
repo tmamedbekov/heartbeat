e5831323e08638382f2888ae871141425a355ba6
joecool2424 <joep24@gmail.com>
2017-10-02 13:51:26 -0500

    Initial Project Push

---
+++ HeartBeat/HeartBeatPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace HeartBeat
+{
+    public partial class HeartBeatPage : ContentPage
+    {
+        public HeartBeatPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
